df6f1e6a809558a984f389d3b26daf077a79aa48
kyyc <chenyuyu0515@gmail.com>
2022-09-14 18:33:38 +0800

    1.add movie model parcelable

--- app/src/main/java/com/app/moviegallery/Service.kt
+++ app/src/main/java/com/app/moviegallery/Service.kt
@@ -4,10 +4,9 @@ import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
+
 
 object Service {
-
     val okHttpClient = OkHttpClient()
     val retrofit: Retrofit = Retrofit.Builder()
         .baseUrl(Credentials.BASE_URL)
--- app/src/main/java/com/app/moviegallery/models/MovieModel.kt
+++ app/src/main/java/com/app/moviegallery/models/MovieModel.kt
@@ -1,11 +1,47 @@
 package com.app.moviegallery.models
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class MovieModel(
     //Model class for movie model
-    val title:String,
-    val poster_path :String,
-    val release_date:String,
+    val title:String?,
+    val poster_path :String?,
+    val release_date:String?,
     val movie_id:Int,
     val vote_average:Float,
-    val movie_overview:String
-)
+    val movie_overview:String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readInt(),
+        parcel.readFloat(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(poster_path)
+        parcel.writeString(release_date)
+        parcel.writeInt(movie_id)
+        parcel.writeFloat(vote_average)
+        parcel.writeString(movie_overview)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<MovieModel> {
+        override fun createFromParcel(parcel: Parcel): MovieModel {
+            return MovieModel(parcel)
+        }
+
+        override fun newArray(size: Int): Array<MovieModel?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
